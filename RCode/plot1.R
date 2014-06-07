fb89405813e1745fe39ba57eab2fb1089e83a93f
sundeepnarang <sundeepnrng@gmail.com>
2014-06-07 14:19:50 -0400

    Plots

---
+++ RCode/plot1.R
@@ -0,0 +1,8 @@
+data <- read.table("household_power_consumption.txt",header=TRUE, col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"), sep=";", na.strings = "?");
+data$Date <- as.Date(data$Date,format ="%d/%m/%Y")
+x <- data$Date == as.Date("2007-02-02") | data$Date == as.Date("2007-02-01") 
+data<-data[x,]
+data$Time<-strptime(paste(data$Date,data$Time),format="%Y-%m-%d %H:%M:%S")
+png(filename="plot1.png")
+hist(data$Global_active_power, col="red", xlab="Global Active Power (kilowatts)",  main="Global Active Power")
+dev.off()
